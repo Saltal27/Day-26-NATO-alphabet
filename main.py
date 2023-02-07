985329d3243a0bfd97408f32646276dffdde2030
Saltal27 <omarmobarak53@gmail.com>
2023-02-07 19:40:25 +0300

    .

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import pandas
 nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
 
-user_word = input("Enter a word: ").upper()
-nato_phonetic_list = [nato_alphabet_dict[letter] for letter in user_word]
+valid_info = True
 
-print(nato_phonetic_list)
+while valid_info:
+    user_word = input("Enter a word: ").upper()
+    try:
+        nato_phonetic_list = [nato_alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        valid_info = False
+        print(nato_phonetic_list)
